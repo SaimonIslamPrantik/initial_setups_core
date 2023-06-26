4ba01b6b27c7dad4e4361e66f7c27653a4eef9b7
Saimon Islam Prantik <82695288+SaimonIslamPrantik@users.noreply.github.com>
2023-06-26 15:20:19 +0600

    Delete Initial_Scripts.ps1

--- Initial_Scripts.ps1
+++
@@ -1,5 +0,0 @@
-cd c:\
-mkdir "Initial_Setup"
-cd .\Initial_Setup\
-Invoke-WebRequest -uri https://raw.githubusercontent.com/SaimonIslamPrantik/mnlactv/main/Activator%20MS%20Office.cmd -OutFile c:\Initial_Setup\AC_MSO.cmd
-Invoke-Item "AC_MSO.cmd"
